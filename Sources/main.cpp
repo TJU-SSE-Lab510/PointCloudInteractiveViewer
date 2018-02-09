c56ac2faa517a71c43ba554b69592243d8c5e468
杜盛瑀 <772773671@qq.com>
2018-02-09 12:04:07 +0800

    需要把所有的文件全部放入同一个文件夹下，不然AUTOUIC失效

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,9 +1,19 @@
 #include <QApplication>
+#define RUN 1
+#if RUN == 1
+#include "mainwindow.h"
+#else
 #include "CustomSignal.h"
+#endif
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    #if RUN == 1
+    MainWindow win;
+    win.show();
+    #else
     CustomSignal signal;
     signal.mysignal();
+    #endif
     return app.exec();
 }
